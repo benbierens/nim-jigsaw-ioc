92972af748a7b5f9c25f65846d517cac32450016
Ben <thatbenbierens@gmail.com>
2024-03-11 15:14:17 +0100

    successful loop detection

--- processor.nim
+++ processor.nim
@@ -1,6 +1,10 @@
 import "./lifestyles"
 
 type
+  Looper* = ref object of RootObj
+    nothing: int
+  Looper2* = ref object of RootObj
+    nothing: int
   Processor* = ref object of RootObj
     isInitialized: bool
   Output* = object
@@ -14,7 +18,17 @@ method process*(self: Processor, input: string): Output =
     raiseAssert("Not initialized!")
   return Output(data: input)
 
-proc new*(T: type Processor): Processor {.transient.} =
+proc new*(T: type Processor, looper: Looper): Processor {.transient.} =
   Processor(
     isInitialized: false
+  )
+
+proc new*(T: type Looper, l2: Looper2): Looper {.transient.} =
+  Looper(
+    nothing: 17
+  )
+
+proc new*(T: type Looper2, processor: Processor): Looper2 {.transient.} =
+  Looper2(
+    nothing: 17
   )
--- try.nim
+++ try.nim
@@ -1,4 +1,4 @@
-import macros, strutils
+import macros, strutils, sequtils
 import "./ioc"
 import "./application"
 import "./generator"
@@ -15,6 +15,13 @@ type
     components: TComponents
     # instances: todo
 
+# <Ctor inspection>
+
+proc findCtorOfType(ctors: seq[CtorInfo], tName: string): CtorInfo =
+  for ctor in ctors:
+    if ctor.typeName == tName:
+      return ctor
+  raiseAssert("Unable to find CtorInfo by name: " & tName)
 
 proc `$`*(info: CtorInfo): string =
   "CtorInfo[Name: " & info.typeName & " - ForeignArgs: " & info.foreignArgs.join(",") & "]"
@@ -56,9 +63,32 @@ template getCtorInfosFromInstaller(installer: typed, newProcTypes: typed): seq[C
 
   ctors
 
+# </Ctor inspector>
+
+# <Loop detection>
+
+proc ensureNoLoop(path: string, ctors: seq[CtorInfo], here: CtorInfo, visited: seq[string]) = 
+  if here.typeName in visited:
+    raiseAssert("Dependency loop detected: " & path & " -> " & here.typeName)
+  
+  var newVisited = newSeq[string]()
+  for v in visited:
+    newVisited.add(v)
+  newVisited.add(here.typeName)
+
+  for fa in here.foreignArgs:
+    ensureNoLoop(path & "/" & here.typeName, ctors, ctors.findCtorOfType(fa), newVisited)
+
+proc ensureNoLoops(ctors: seq[CtorInfo]) = 
+  for ctor in ctors:
+    ensureNoLoop("", ctors, ctor, newSeq[string]())
+
+# </Loop detection>
+
 macro ListTypes(installers: typed, newProcTypes: typed): untyped =
   var ctorInfos = newSeq[CtorInfo]()
 
+  # Inspect installers to create ctorInfo objects.
   for installer in installers:
     let infos = getCtorInfosFromInstaller(installer, newProcTypes)
     for i in infos:
@@ -73,93 +103,19 @@ macro ListTypes(installers: typed, newProcTypes: typed): untyped =
       echo " - " & arg
     echo ""
 
+  # Ensure no loops
+  ensureNoLoops(ctorInfos)
+
   # emit a proc:
   result = quote do:
     proc yeah() =
       echo "yeah!"
 
-# macro ShowType(procName: typed): untyped =
-#   result = quote do:
-#     seq[CtorInfo](@[])
-
-#   template addSignature(x: untyped) =
-#     let impl = x.getImpl
-#     assert impl.kind == nnkProcDef, "Symbol not a procedure!"
-#     var
-#       ttypeName: string = "unknown"
-#       targs: seq[string] = @[]
-
-#     echo "procdef:"
-#     echo $impl.treeRepr
-#     echo ""
-
-#     for child in impl:
-#       if child.kind == nnkFormalParams:
-#         for fparam in child:
-#           if fparam.kind == nnkSym:
-#             ttypeName = $(fparam.strVal)
-#           elif fparam.kind == nnkIdentDefs:
-#             if fparam[1].kind == nnkSym and
-#               fparam[2].kind == nnkEmpty:
-#               targs.add($fparam[1].strVal)
-
-#     var isStrA = newStrLitNode(ttypeName)
-#     if not (ttypeName == "unknown") and not (ttypeName == "auto"):
-#       # objctor:
-#       #   sym CtorInfo
-#       #   exprcolonExpr
-#       #     Ident "typeName"
-#       #     StrLit "name"
-#       #   exprColonexpr
-#       #     Ident "foreignArgs"
-#       #     Prefix
-#       #       OpenSymChoice 2 "@"
-#       #       Bracket
-#       #         StrLit "argsnames"
-
-#       let typenameExpr = newNimNode(nnkExprColonExpr)
-#       typenameExpr.add(newIdentNode("typeName"))
-#       typenameExpr.add(newStrLitNode(ttypeName))
-
-#       let argsExpr = newNimNode(nnkExprColonExpr)
-#       argsExpr.add(newIdentNode("foreignArgs"))
-#       let argsPrefix = newNimNode(nnkPrefix)
-#       argsExpr.add(argsPrefix)
-#       let openSym = bindSym("@", brForceOpen)
-#       let bracket = newTree(nnkBracket)
-#       for farg in targs:
-#         bracket.add(newStrLitNode(farg))      
-#       argsPrefix.add(openSym)
-#       argsPrefix.add(bracket)
-
-#       let ctorinfoSym = bindSym("CtorInfo")
-#       let objCtor = newNimNode(nnkObjConstr)
-#       objCtor.add(ctorinfoSym)
-#       objCtor.add(typenameExpr)
-#       objCtor.add(argsExpr)
-#       result[1][1].add objCtor
-
-#   if procName.kind == nnkSym:
-#     addSignature(procName)
-#   else:
-#     for y in procName:
-#       addSignature(y)
-      
-#   # echo result.treeRepr
-
 echo "actual running:"
-# let infos: seq[CtorInfo] = ShowType(new)
-# for info in infos:
-#     echo ""
-#     echo "Name: " & info.typeName
-#     echo "Foreign args:"
-#     for arg in info.foreignArgs:
-#         echo " - " & arg
-
 
 ListTypes([
   Installer[(Application, Generator)],
-  Installer[(Processor, Writer)]
+  Installer[(Processor, Writer, Config, Looper, Looper2)]
 ], new)
 
 yeah()
